a3c3aeec6c42fa993b24b0aa641ddeb7b1cf0531
Adrien Wehrung <a.wehrung@finatix.de>
2021-01-22 15:18:01 +0100

    make client reactive

--- example-client/src/main/java/com/example/spring_reactive/client/controller/TriggerController.java
+++ example-client/src/main/java/com/example/spring_reactive/client/controller/TriggerController.java
@@ -1,16 +1,15 @@
 package com.example.spring_reactive.client.controller;
 
-import com.example.spring_reactive.api.java_client.handler.ApiException;
 import com.example.spring_reactive.api.java_client.handler.ThingsApi;
 import com.example.spring_reactive.api.java_client.model.Thing;
 import com.example.spring_reactive.client.converter.ThingConverter;
-import lombok.SneakyThrows;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,16 +27,15 @@ public class TriggerController {
         this.thingsApi = thingsApi;
     }
 
-    @SneakyThrows(ApiException.class)
     @GetMapping("/trigger/naive")
     public ResponseEntity<List<String>> getThingsFromServer() {
         log.info("Begin of Controller, requesting {} things", NB_THINGS_TO_REQUEST);
-        List<Thing> things = thingsApi.listThings(5);
+        Flux<Thing> things = thingsApi.listThings(5);
         log.info("Received things from Server, beginning extraction process");
         return new ResponseEntity<>(
-                things.stream()
-                        .map(ThingConverter::extractContent)
-                        .collect(Collectors.toList()),
+                things.map(ThingConverter::extractContent)
+                        .collect(Collectors.toList())
+                        .block(),
                 HttpStatus.OK
         );
     }
--- example-server/src/main/java/com/example/spring_reactive/server/service/ThingsService.java
+++ example-server/src/main/java/com/example/spring_reactive/server/service/ThingsService.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -15,7 +16,21 @@ public class ThingsService {
     private static final Logger log = LogManager.getLogger(ThingsService.class);
 
     public Flux<Thing> getThings(Integer nbThings) {
-        log.info("Begin processing request for {} things", nbThings);
+        //return this.getThingsFromExistingStream(nbThings);
+        return this.getThingsFromLiveEvents(nbThings);
+    }
+
+    public Flux<Thing> getThingsFromLiveEvents(Integer nbThings) {
+        log.info("Begin processing request for {} things, using live events", nbThings);
+        return Flux.create(emitter -> {
+            this.getRawThings(nbThings)
+                    .forEach(rawThing -> emitter.next(ThingFactory.fromContent(rawThing)));
+            emitter.complete();
+        }, FluxSink.OverflowStrategy.BUFFER);
+    }
+
+    public Flux<Thing> getThingsFromExistingStream(Integer nbThings) {
+        log.info("Begin processing request for {} things, using existing stream", nbThings);
         return Flux.fromStream(this.getRawThings(nbThings))
                 .log()
                 .map(ThingFactory::fromContent);
